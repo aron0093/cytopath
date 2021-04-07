ba1bff1f6235f4d937768ae0bd7b8142d4399d00
rgupta <revant.gupta@imsb.biol.ethz.ch>
2021-04-07 08:36:41 +0000

    Removed the key parameter.

--- cytopath/plotting.py
+++ cytopath/plotting.py
@@ -1,8 +1,8 @@
 from .plotting_functions.plot_alignment import pl_cytopath_alignment
 from .plotting_functions.plot_sampling import pl_cytopath_sampling
 
-def plot_alignment(data, directory='', basis='', name='', size=10, figsize=(12,3), smoothing=False):
-    pl_cytopath_alignment(data, folder=directory, basis=basis, key=name, size=size, figsize=figsize, smoothing=smoothing)
+def plot_alignment(data, directory='', basis='', size=10, figsize=(12,3), smoothing=False):
+    pl_cytopath_alignment(data, folder=directory, basis=basis, size=size, figsize=figsize, smoothing=smoothing)
 
 def plot_sampling(data, directory='', basis=''):
     pl_cytopath_sampling(data, folder=directory, basis=basis)
