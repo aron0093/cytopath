3c86d262c1693e52ecabe94691b3f0f7a98482d8
rgupta <revant.gupta@imsb.biol.ethz.ch>
2021-04-20 16:15:53 +0000

    Plotting function was renamed to plot_trajectories.

--- cytopath/__init__.py
+++ cytopath/__init__.py
@@ -2,5 +2,5 @@ from . import anndata
 from . import scvelo
 from .markov_chain_sampler import sampling
 from .trajectory_inference import trajectories, merge_steps
-from .plotting import plot_alignment, plot_sampling
+from .plotting import plot_trajectories, plot_sampling
 from .utils import read_processed
