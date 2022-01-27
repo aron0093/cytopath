5fc1dedcce0fb1d95339c8625a072e33c3d5983b
Revant Gupta <25486108+aron0093@users.noreply.github.com>
2022-01-27 21:40:00 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+github_repo = "cytopath"
+github_nb_repo = "cytopath-notebooks"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
