9fc89c6ee9b351a7c2e6e40a5a00ccf7dc654a39
aron0093 <revant.gupta.93@gmail.com>
2022-02-13 18:43:04 +0000

    Added algorithm for automatic selection of technical parameters.

--- cytopath/markov_chain_sampler.py
+++ cytopath/markov_chain_sampler.py
@@ -49,8 +49,8 @@ def markov_sim(j, sim_number, max_steps, root_cells, clusters, trans_matrix, tra
             clust_state[k, i] = clusters[currState[k, i]]
     return currState, np.sum(-np.log(prob_state), axis=1), clust_state
 
-def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=10, min_sim_ratio=1.0, traj_number=500, sim_number=500,
-                end_point_probability=0.99, root_cell_probability=0.99, end_points=[], root_cells=[], end_clusters = [], root_clusters=[], min_clusters=3,
+def sampling(data, auto_adjust=True, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=10, min_sim_ratio=0.6, rounds_limit=10, traj_number=500, sim_number=500,
+                end_point_probability=0.99, root_cell_probability=0.99, end_points=None, root_cells=None, end_clusters=None, root_clusters=None, min_clusters=3,
                 normalize=False, unique=True, num_cores=1, copy=False):
     
     """Markov sampling of cell sequences starting from defined root cells to defined terminal regions based on a cell-cell transition probability matrix.
@@ -61,23 +61,23 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
         Annotated data matrix with transition probabilities, end points and clustering.
     matrix_key: Key for accessing transition matrix stored in adata.uns
     cluster_key: Clustering of cells to use for terminal region annotation.  
-    max_steps: `integer` (default: 30)
+    max_steps: `integer` (default: 10)
         Maximum number steps of simulation.
     traj_number: `integer`(default: 500)
         Minimum number of samples generated for each terminal region.
     sim_number: `integer`(default: 500)
         Initial number of samples generated for each terminal region.
-    end_point_probability: 'float' (default:0.95)
+    end_point_probability: 'float' (default:0.99)
         End point probability threshold at which a cell is considered to be a terminal state for samples.
-    root_state_probability: 'float' (default:0.95)
+    root_state_probability: 'float' (default:0.99)
         Start state probability threshold at which a cell is considered to be an origin for samples.
-    end_points: `list` (default: [])
+    end_points: `list` (default: None)
         Numerical indices of the cells considered to be end points for manual annotation. Precendence over end_point_probability and end_clusters.
-    root_cells: `list` (default: [])
+    root_cells: `list` (default: None)
         Numerical indices of the cells considered to be root states for manual annotation. Precendence over root_state_probability and root_clusters.
-    end_clusters: `list` (default: [])
+    end_clusters: `list` (default: None)
         Cluster IDs to be considered end points. Precendence over end_point_probability.
-    root_clusters: `list` (default: [])
+    root_clusters: `list` (default: None)
         Cluster IDs to be considered root states. Precendence over root_state_probability.
     min_clusters: `integer` (default:3)
         Minium number of clusters covered by each simulation. (cluster_key)
@@ -123,9 +123,14 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
     
     if sparse.issparse(adata.X):
         adata.X = np.asarray(adata.X.toarray())
-    
-    # Check if matrix has been normalized
-    if normalize == False:
+
+    # Row sum normalization 
+    if normalize:
+        new_array = trans_matrix
+        normalization_v = new_array.sum(axis=1)
+        for i in range(len(normalization_v)):
+            trans_matrix[i,:]=trans_matrix[i,:]/normalization_v[i]
+    else: # Check if matrix has been normalized
         row_sums = np.sum(trans_matrix, axis=1)
         try: 
             assert max(row_sums) <= 1.0 + 1e-3
@@ -133,13 +138,6 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
         except AssertionError:
             raise ValueError('Transition matrix has not been normalized. Either pass normalize==True or provide a row sum normalized matrix.')
 
-    # Row sum normalization 
-    if normalize == True:
-        new_array = trans_matrix
-        normalization_v = new_array.sum(axis=1)
-        for i in range(len(normalization_v)):
-            trans_matrix[i,:]=trans_matrix[i,:]/normalization_v[i]
-        
     # Check if cluster labels are provided
     try:
         adata.obs[cluster_key]
@@ -150,7 +148,21 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
     # Use louvain warning
     if cluster_key != 'louvain':
         warnings.warn('A finely resolved clustering is required to identify terminal regions. Consider using louvain clustering.', UserWarning)
+    
+    # Check if root and end cells are lists
+    
+    try: not root_cells
+    except: root_cells = list(root_cells)
 
+    try: not end_points
+    except: end_points = list(end_points)
+
+    try: not root_clusters
+    except: root_clusters = list(root_clusters)
+
+    try: not end_clusters
+    except: end_clusters = list(end_clusters)
+    
     ### Checks Done
 
     # Define root and end points, clusters
@@ -160,8 +172,8 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
     trans_matrix_indice_zeros, trans_matrix_probabilites_zeros = matrix_prep(trans_matrix)
 
     # Retrieve all root cells
-    if len(root_cells) == 0:
-        if len(root_clusters) == 0:
+    if not root_cells:
+        if not root_clusters:
             try:
                 root_cells = np.asarray(np.where(np.asarray(adata.obs["root_cells"]) >= root_cell_probability))[0]
             except:
@@ -171,8 +183,8 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
     adata.uns['run_info']['root_cells'] = root_cells
 
     # Retrieve all end point cells
-    if len(end_points) == 0:
-        if len(end_clusters) == 0:
+    if not end_points:
+        if not end_clusters:
             try:
                 end_points = np.asarray(np.where(np.asarray(adata.obs["end_points"]) >= end_point_probability))[0]
                 #TODO
@@ -200,18 +212,36 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
     end_point_clusters = adata.obs[cluster_key][end_points].astype(str).values
     end_clusters_ = np.unique(end_point_clusters)
     
+    # Adjust simulation parameters based on data
+    if auto_adjust:
+        print('Adjusting simulation parameters based on dataset properties. Set auto_adjust=False if this is unwanted.')
+
+        # Initial number of simulations
+        traj_number = math.ceil(np.log10(adata.shape[0])*500) # Dataset size 
+        print('Number of required simulations per end point (traj_number) set to {}'.format(traj_number))
+        sim_number = traj_number*len(end_clusters_)  # number of terminal regions  
+        print('Number of initial simulations (sim_number) set to {}'.format(sim_number))
+
+        # Initial number of simulation steps
+        max_steps = math.ceil(np.log10(adata.shape[0])*5) # 10, 15, 20 steps for a dataset of 100, 1000, 10000 cells
+        print('Number of initial simulation steps (max_steps) set to {}'.format(max_steps)) 
+
     # Initialize all empty lists
-    traj_num = [0]
     glob_all_seq = []
     glob_prob_all_seq = []
     glob_cluster_seq_all = []
     sim_number_ = sim_number
 
-    count = 1
-    # Resample samples until the number of trajectories for each end point has been reached.
-    while min(traj_num) < traj_number:
+    # Define variables for conditional
+    traj_num = [0]
+    ratio_obtained = 0
 
-        print("Sampling round " + str(count))
+    count = 0 # Iterate sampling runs
+    # Resample samples until the number of trajectories for each end point has been reached.
+    while (min(traj_num) < traj_number) or (ratio_obtained < 0.1):
+
+        print()
+        print("Sampling round: {}".format(count))
         count+=1
 
         # Parallelisation over the cells: 1 Thread for the samples of each cell.
@@ -242,7 +272,7 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
         cluster_seq_all = [cluster_seq_all[i] for i in sorted(indices)]
 
         # If unique, then only keep unique samples
-        if unique == True:
+        if unique:
             indices = np.unique(all_seq, axis=0, return_index=True)[1]
             all_seq = [all_seq[i] for i in sorted(indices)]
             prob_all_seq = [prob_all_seq[i] for i in sorted(indices)]
@@ -269,23 +299,51 @@ def sampling(data, matrix_key = 'T_forward', cluster_key = 'louvain', max_steps=
             all_seq_cluster = all_seq_cluster[0]
             traj_num.append(len(all_seq_cluster))
         
-        # Re adjust number of simulation steps and resample if required.
+        # Increase sampling steps till min_sim_ratio of simulations end at a end point
+        ratio_obtained = np.round(sum(traj_num)/(sim_number_*len(root_cells)), 6)
+        ratio_min = np.round(min(traj_num)/traj_number, 4)
+
+        if count==rounds_limit:
+            print('{} % of required simulations obtained for lagging end point {}.'.format(np.round(ratio_min*100, 2),
+                                                                                                        end_clusters_[np.argmin(traj_num)]))
+            print('{} % of simulations reached atleast one endpoint after {} rounds.'.format(np.round(ratio_obtained*100, 4), 
+                                                                                                rounds_limit))
+            raise ValueError('Sampling failed. Try lowering min_sim_ratio or increase rounds_limit.')
+
         if min(traj_num) < min_sim_ratio*traj_number:
-            if count == 5:
-                raise ValueError('Insufficient samples obtained after 5 rounds. Try increasing max steps.')
-            else:
-                max_steps = int(2*(max_steps)+10) # Offset adds stability if starting value is too low
+            max_steps = math.ceil(traj_number/(max(max_steps, min(traj_num))+1)) + max_steps
 
-                glob_all_seq = []
-                glob_prob_all_seq = []
-                glob_cluster_seq_all = []
+            glob_all_seq = []
+            glob_prob_all_seq = []
+            glob_cluster_seq_all = []
 
-                print('Insufficient proportion of requested samples obtained. Increasing simulation steps to {}'.format(max_steps))
-                continue
+            print('{} % of required simulations obtained for lagging end point {}. This is less than min_sim_ratio.'.format(np.round(ratio_min*100, 2),
+                                                                                                        end_clusters_[np.argmin(traj_num)]))
+            print('{} % of simulations reached atleast one endpoint. Increasing number of simulation steps to {}.'.format(np.round(ratio_obtained*100, 4), max_steps))
+            continue
 
-        # Alternatively just perform more sampling (Feature active if min_sim_ratio < 1)    
-        sim_number_ = math.ceil(traj_number/(min(traj_num) + 1))*sim_number
-    
+        #TODO: Better heuristic for this
+        if ratio_obtained < 0.1:
+            max_steps = 2*max_steps
+
+            glob_all_seq = []
+            glob_prob_all_seq = []
+            glob_cluster_seq_all = []
+
+            print('{} % of required simulations obtained for lagging end point {}.'.format(np.round(ratio_min*100, 2),
+                                                                                                        end_clusters_[np.argmin(traj_num)]))
+            print('{} % of simulations reached atleast one endpoint. Increasing number of simulation steps to {}.'.format(np.round(ratio_obtained*100, 4), max_steps))
+            continue
+
+        # Alternatively perform more sampling (Feature active if min_sim_ratio<1)    
+        if min(traj_num) < traj_number:
+            print('{} % of required simulations obtained for lagging end point {}.'.format(np.round(ratio_min*100, 2),
+                                                                                                            end_clusters_[np.argmin(traj_num)]))
+            print('{} % of simulations reached atleast one endpoint. Generating {} additional samples.'.format(np.round(ratio_obtained*100, 4), sim_number_))
+            continue
+
+    print('Sampling done.')
+
     # Retrieve traj_number of samples for each end point.
     glob_prob_all_seq = np.concatenate(glob_prob_all_seq)
     all_seq=np.vstack(glob_all_seq)
