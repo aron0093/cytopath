197939c2ca33685339b2b125ae12de458014fe28
aron0093 <revant.gupta.93@gmail.com>
2022-02-14 02:52:58 +0000

    Radial heatmap for circular processes.

--- cytopath/plotting.py
+++ cytopath/plotting.py
@@ -1,5 +1,6 @@
 from .plotting_functions.plot_alignment import pl_cytopath_alignment
 from .plotting_functions.plot_sampling import pl_cytopath_sampling
+from .plotting_functions.plot_radial_heatmap import radial_heatmap
 
 def plot_trajectories(data, directory='', basis='', size=10, figsize=(12,3), save_type='svg', smoothing=False):
     pl_cytopath_alignment(data, folder=directory, basis=basis, size=size, figsize=figsize, smoothing=smoothing, save_type=save_type)
