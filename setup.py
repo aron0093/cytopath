2178db25b703b68c14b4400c8e9ad7a7925401cd
aron0093 <revant.gupta.93@gmail.com>
2023-07-18 00:20:09 +0000

    Release v0.2.0

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
   name='cytopath',
-  version='0.1.9',
+  version='0.2.0',
   description='Simulation based inference of differentiation trajectories from RNA velocity fields.',
   license='BSD 3-Clause License',
   packages=find_packages(),
   author = 'Revant Gupta',
   author_email = 'revant.gupta.93@gmail.com',
   url = 'https://github.com/aron0093/cytopath',
-  download_url = 'https://github.com/aron0093/cytopath/archive/v_019.tar.gz',
+  download_url = 'https://github.com/aron0093/cytopath/archive/v_020.tar.gz',
   keywords = ['Trajectory Inference', 'single-cell RNA sequencing', 'RNA velocity'],
   install_requires=[
-          'numpy>=1.20.0',
-	        'pandas>=1.4.1',
-          'scipy',
-          'anndata',
           'scvelo>=0.1.25',
+          'numpy==1.23.5',
           'joblib',
           'fastdtw',
           'hausdorff',
@@ -29,6 +26,5 @@ setup(
     'Intended Audience :: Science/Research',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ])
